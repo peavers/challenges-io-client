5146aca583d38bff73cbf45ff9c0ff9d93afb695
Chris Turner <chris@forloop.space>
2019-09-25 22:20:24 +0100

    clean: remove custom loading manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { SharedModule } from './shared';
+import { BrowserModule } from '@angular/platform-browser';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { CoreModule } from './core/core.module';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { environment } from '../environments/environment';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
+import { LoginComponent } from './layout/login/login.component';
 import { MaterialModule } from './shared/material.module';
+import { MatProgressButtonsModule } from 'mat-progress-buttons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { SharedModule } from './shared';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 import md from 'highlight.js/lib/languages/markdown';
 import yaml from 'highlight.js/lib/languages/yaml';
@@ -19,21 +25,6 @@ import scss from 'highlight.js/lib/languages/scss';
 import typescript from 'highlight.js/lib/languages/typescript';
 import javascript from 'highlight.js/lib/languages/javascript';
 import java from 'highlight.js/lib/languages/java';
-import scala from 'highlight.js/lib/languages/scala';
-import go from 'highlight.js/lib/languages/go';
-
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { MatProgressButtonsModule } from 'mat-progress-buttons';
-
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
-import { LoginComponent } from './layout/login/login.component';
-import { NgxPageScrollModule } from 'ngx-page-scroll';
-
-import { LMarkdownEditorModule } from 'ngx-markdown-editor';
-import { LoadingScreenInterceptor } from './shared/helper/loading.interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 export function hljsLanguages() {
   return [
@@ -43,9 +34,7 @@ export function hljsLanguages() {
     { name: 'scss', func: scss },
     { name: 'xml', func: xml },
     { name: 'yml', func: yaml },
-    { name: 'md', func: md },
-    { name: 'scala', func: scala },
-    { name: 'go', func: go }
+    { name: 'md', func: md }
   ];
 }
 
@@ -61,16 +50,10 @@ export function hljsLanguages() {
     BrowserAnimationsModule,
     MatProgressButtonsModule,
     LMarkdownEditorModule,
-
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoadingScreenInterceptor,
-      multi: true
-    },
     {
       provide: HIGHLIGHT_OPTIONS,
       useValue: {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,7 +8,6 @@ import { DeleteConfirmDialogComponent } from './component/delete-confirm-dialog/
 import { FeedbackDialogComponent } from './component/feedback-dialog/feedback-dialog.component';
 import { LMarkdownEditorModule } from 'ngx-markdown-editor';
 import { NgxMdModule } from 'ngx-md';
-import { LoadingScreenComponent } from './component/loading-screen/loading-screen.component';
 import { UserPictureComponent } from './component/user-picture/user-picture.component';
 
 @NgModule({
@@ -26,7 +25,6 @@ import { UserPictureComponent } from './component/user-picture/user-picture.comp
     DeleteConfirmDialogComponent,
     ScrollToTopComponent,
     FeedbackDialogComponent,
-    LoadingScreenComponent,
     UserPictureComponent
   ],
   exports: [
@@ -35,7 +33,6 @@ import { UserPictureComponent } from './component/user-picture/user-picture.comp
     ReactiveFormsModule,
     RouterModule,
     ScrollToTopComponent,
-    LoadingScreenComponent,
     UserPictureComponent
   ],
   entryComponents: [DeleteConfirmDialogComponent, FeedbackDialogComponent],
